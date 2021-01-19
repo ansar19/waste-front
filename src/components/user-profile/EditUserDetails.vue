ab6f1f7aead359ea3ed3ce2c0a274dffe495b697
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-19 19:00:48 +0600

    Create EditUserDetails.vue
    editUser component

---
+++ src/components/user-profile/EditUserDetails.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="new">
+    <form>
+      <div class="form-group">
+        <label >Username:</label>
+        <input type="text" class="form-control" v-model="username" placeholder="Ex: John">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label >Role:</label>
+        <input type="text" class="form-control" v-model="role" placeholder="Ex: Administration">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "EditUserDetails",
+  data(){
+    return{
+      username: 'John',
+      role: 'Administration'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.new{
+  margin: auto;
+  width: 50%;
+}
+</style>
