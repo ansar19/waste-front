30fb5e48b1541362ffc75194fb61db67ff8e48d8
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-19 19:48:54 +0600

    Create NewUser.vue
    NewUser component

---
+++ src/components/user/NewUser.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+      <form>
+        <div class="form-group">
+          <label>Имя:</label>
+          <input type="text" class="form-control" placeholder="Иванов Иван">
+        </div>
+        <div class="form-group">
+          <label>Должность:</label>
+          <input type="text" class="form-control" placeholder="Директор">
+        </div>
+        <button type="submit" class="btn btn-primary">Добавить</button>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewUser',
+  data() {
+    return {
+      newUser:{
+        title: '',
+        body: ''
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.container{
+  margin: auto;
+  width: 50%;
+}
+</style>
