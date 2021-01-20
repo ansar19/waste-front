290cfbaae867627f026b14c4de7b670117ab95ab
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-20 21:16:17 +0600

    Update Sites.vue

--- src/views/Sites.vue
+++ src/views/Sites.vue
@@ -1,156 +1,41 @@
 <template>
-  <div>
-    <vue-good-table
-      :columns="columns"
-      :rows="sites"
-      :line-numbers="true"
-      :search-options="{
-        enabled: true,
-        placeholder: 'Введите текст для поиска',
-      }"
-      :pagination-options="paginationOptions"
-      compactMode
-      >
-
-      <div slot="table-actions">
-        <router-link :to="{path:'/new-site/'}"><button class="btn btn-outline-primary">Create</button></router-link>
-        <button class="btn btn-outline-primary" ><i class="fa fa-download"></i></button>
+  <div class="main-content-container container-fluid px-4">
+    <!-- Page Header -->
+    <div class="page-header row no-gutters py-4">
+      <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+        <span class="text-uppercase page-subtitle">
+          Список участков</span
+        >
+        <h3 class="page-title">Участки:</h3>
       </div>
-
-       <template slot="table-row" slot-scope="props">
-        <span v-if="props.column.field == 'action'">
-          <router-link to="/edit-site">
-            <button class="btn-primary btn-small btn">
-              <i class="fa fa-edit"></i>
-            </button>
-          </router-link>
-          <router-link to="/show-site">
-            <button class="btn-info btn-small btn">
-              <i class="fa fa-eye"></i>
-            </button>
-          </router-link>
-          <button class="btn-danger btn-small btn">
-            <i class="fa fa-trash"></i>
-          </button>
-        </span>
-      </template>
-
-    </vue-good-table>
+    </div>
+    <!-- Default Light Table -->
+    <div class="row">
+      <div class="col">
+        <div class="card card-small mb-4">
+          <div class="card-body">
+            <SitesTable />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import 'vue-good-table/dist/vue-good-table.css'
-import { VueGoodTable } from 'vue-good-table';
-// import gql from "graphql-tag"
-// const AllPosts = gql`
-//   query (
-//     $options: PageQueryOptions
-//   ) {
-//     posts(options: $options) {
-//       data {
-//         id
-//         title
-//         body
-//       }
-//     }
-//   }
-// `
+import SitesTable from '@/components/SitesTable';
+
 export default {
-  name: 'Sites',
+  data() {
+    return {
+    };
+  },
   components: {
-    VueGoodTable,
+    SitesTable,
   },
-  data(){
-    return{
-      columns:[
-        {
-         label: 'SiteName',
-         field: 'siteName',
-        },
-        {
-          label: 'SiteComments',
-          field: 'siteComments',
-        },
-        {
-          label: 'Action',
-          field: 'action',
-        },
-      ],
-      sites: [
-        {
-          id: 1,
-          siteName: 'Kazakhstan',
-          siteComments: 'qwertyuiop'
-        },
-        {
-          id: 2,
-          siteName: 'Russia',
-          siteComments: 'qwertyuiop'
-        },
-        {
-          id: 3,
-          siteName: 'USA',
-          siteComments: 'qwertyuiop'
-        }
-      ],
-      paginationOptions: {
-        enabled: true,
-        mode: 'pages',
-        perPage: 5,
-        position: 'bottom',
-        perPageDropdown: [5, 10, 20],
-        dropdownAllowAll: false,
-        setCurrentPage: 1,
-        nextLabel: '>',
-        prevLabel: '<',
-        rowsPerPageLabel: 'Строк на странице',
-        ofLabel: '/',
-        pageLabel: 'стр.', // for 'pages' mode
-        allLabel: 'Все',
-      },
-      // deletePostMutation:`mutation ($id: ID!) 
-      //   {
-      //     deletePost(id: $id)
-      //   }` 
-     }
-  },
-  // apollo:{
-  //   posts:{
-  //     query: AllPosts,
-  //   }
-  // },
-  // methods:{
-  //    deletePost(id) {
-  //     console.log(id)
-  //     this.$apollo.mutate({
-  //       mutation: gql`${this.deletePostMutation}`,
-  //       variables: {
-  //         id: id,
-  //       },
-  //       update: (store) => {
-  //         const queries = [
-  //           { query: AllPosts },
-  //           { query: AllPosts, variables: { filter: { done: this.post } } },
-  //         ]
-  //         const data = queries.map(query => store.readQuery(query))
-  //         data.forEach(({ posts: list }) => {
-  //           console.log(list)
-  //           const index = list.data.findIndex(o => o.id === id)
-  //           if (index !== -1) {
-  //             list.data.splice(index, 1)
-  //           }
-  //         })
-  //         queries.forEach((query, index) => {
-  //           store.writeQuery({
-  //             ...query,
-  //             data: data[index],
-  //           })
-  //         })
-  //       },
-  //     });  
-     
-  //   }
-  // }
-}
+};
 </script>
+
+<style scoped>
+
+</style>
