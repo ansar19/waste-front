d49c24f7868cd44ba72dcc2105c117c5be7d75eb
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-20 15:40:11 +0600

    Create NewSite.vue
    NewSite component

---
+++ src/components/sites/NewSite.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="createPage">
+        <h1>CreatePage</h1>
+       <form>
+        <div class="form-group">
+            <label>siteName</label>
+            <input  class="form-control"  placeholder="Mangystau">
+        </div>
+        <div class="form-group">
+            <label>siteComments</label>
+            <textarea class="form-control" rows="3" placeholder="Desert area"></textarea>
+        </div>
+       </form>
+       <button class="btn btn-outline-primary">Create</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'NewSite',
+}
+
+</script>
+
+<style>
+.createPage{
+    width: 40%;
+    margin: auto;
+}
+</style>
