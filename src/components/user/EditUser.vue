e95db9609479cd8f279d3840ef072291bacb8574
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-21 19:00:00 +0600

    Update EditUser.vue

--- src/components/user/EditUser.vue
+++ src/components/user/EditUser.vue
@@ -1,57 +1,62 @@
 <template>
-  <div class="container-fluid px-4 pb-4">
-    <div class="row">
-      <div class="col-12 md-12">
-        <div class="card-small">
-          <div class="card-header border-bottom">
-            <h5 class="m-0">Переименовать</h5>
-          </div>
+  <d-container fluid class="main-content-container px-4 pb-4">
+    <d-row no-gutters class="page-header py-4">
+      <d-col lg="12" md="12">
+        <d-card class="card-small">
+          <!-- Form Example -->
+          <d-card-header class="border-bottom">
+            <h6 class="m-0">Редактировать </h6>
+          </d-card-header>
 
-          <div class="row ">
-            <div class="col ">
-              <div class="form m-3">
-                <div class="form-group">
-                  <label for="user-name">Имя пользователя: </label>
-                  <input id="user-name" class="form-control" placeholder="" v-model="userName"/>
-                </div>
+          <d-list-group flush>
+            <d-list-group-item class="p-3">
+              <d-row>
+                <d-col>
+                  <d-form>
+                    <div class="form-group">
+                      <label for="user-name"
+                        >Имя пользователя: </label
+                      >
+                      <d-input
+                        id="user-name"
+                        placeholder="Иван Иванов"
+                        v-model="userName"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label for="created-at"
+                        >Зарегистрирован: </label
+                      >
+                      <d-input
+                        id="created-at"
+                        placeholder="2011-01-01"
+                        v-model="createdAt"
+                      />
+                    </div>
 
-                <div class="form-group">
-                  <label for="user-comments">Должность :</label>
-                  <input type="text" id="user-comments" class="form-control" placeholder="" v-model="userComments"/>
-                </div>
-              </div>
-              <div class="card-footer text-muted">
-                <div>
-                  <div class="d-flex">
-                    <button @click="goBack" class="btn btn-outline">Отменить</button>
-                    <button class="btn btn-success ml-auto" type="submit">Сохранить</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+                   
+
+                    <button type="button" class="btn btn-outline-success">
+                      Сохранить
+                    </button>
+                  </d-form>
+                </d-col>
+              </d-row>
+            </d-list-group-item>
+          </d-list-group>
+        </d-card>
+      </d-col>
+    </d-row>
+  </d-container>
 </template>
 
 <script>
 export default {
-  name: 'EditUser',
   data() {
     return {
       userName: 'Иван Иванов',
-      userComments: 'Директор',
+      createdAt: '2011-01-01',
     };
-  },
-  methods: {
-    goBack() {
-      this.$router.go(-1);
-    },
   },
 };
 </script>
-<style scoped>
-
-</style>
