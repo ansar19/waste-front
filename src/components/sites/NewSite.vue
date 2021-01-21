92ba8220c14e5cf7e92db61a2a348f7e1f17d5e9
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-21 18:58:08 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -1,44 +1,68 @@
 <template>
-  <div class="container-fluid px-4 pb-4">
-    <div class="row">
-      <div class="col-12 md-12">
-        <div class="card-small">
-          <div class="card-header border-bottom">
-            <h5 class="m-0">Новый участок : </h5>
-          </div>
+  <d-container fluid class="main-content-container px-4 pb-4">
+    <d-row no-gutters class="page-header py-4">
+      <d-col lg="12" md="12">
+        <d-card class="card-small">
+          <!-- Form Example -->
+          <d-card-header class="border-bottom">
+            <h6 class="m-0">Новый участок</h6>
+          </d-card-header>
 
-          <div class="row ">
-            <div class="col ">
-              <div class="form m-3">
-                <div class="form-group">
-                  <label for="site-name">Название участка : </label>
-                  <input id="site-name" class="form-control" placeholder="Введите название участка" v-model="siteName"/>
-                </div>
+          <d-list-group flush>
+            <d-list-group-item class="p-3">
+              <d-row>
+                <d-col>
+                  <d-form>
+                    <div class="form-group">
+                      <label for="site-name"
+                        >Наименование участка: </label
+                      >
+                      <d-input
+                        id="site-name"
+                        placeholder="Введите название участка"
+                        v-model="siteName"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label for="site-comments"
+                        >Комментарии: </label
+                      >
+                      <d-input
+                        id="site-comments"
+                        placeholder="Введите комментарии"
+                        v-model="siteComments"
+                      />
+                    </div>
 
-                <div class="form-group">
-                  <label for="site-comments">Коментарии : </label>
-                  <input type="text" id="site-comments" class="form-control" placeholder="Введите комментарии" v-model="siteComments"/>
-                </div>
-              </div>
-              <div class="card-footer text-muted">
-                <div>
-                  <div class="d-flex">
-                    <button @click="goBack" class="btn btn-outline">Отменить</button>
-                    <button class="btn btn-success ml-auto" type="submit">Сохранить</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+                    
+
+                    <div class="card-footer text-muted">
+                      <d-card-footer>
+                        <div class="d-flex">
+                          <a href="#" @click="goBack()" class="stretched-link"
+                            >Отменить</a
+                          >
+                          <d-button
+                            class="btn btn-success ml-auto"
+                            type="submit"
+                            >Сохранить</d-button
+                          >
+                        </div>
+                      </d-card-footer>
+                    </div>
+                  </d-form>
+                </d-col>
+              </d-row>
+            </d-list-group-item>
+          </d-list-group>
+        </d-card>
+      </d-col>
+    </d-row>
+  </d-container>
 </template>
 
 <script>
 export default {
-  name: 'NewSite',
   data() {
     return {
       siteName: '',
@@ -52,6 +76,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-
-</style>
