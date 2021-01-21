6c7895fac21229a48fd38858436e32d4f4bb8e47
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-21 17:08:59 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -1,31 +1,57 @@
 <template>
-    <div class="createPage">
-        <h1>CreatePage</h1>
-       <form>
-        <div class="form-group">
-            <label>siteName</label>
-            <input  class="form-control"  placeholder="Mangystau">
+  <div class="container-fluid px-4 pb-4">
+    <div class="row">
+      <div class="col-12 md-12">
+        <div class="card-small">
+          <div class="card-header border-bottom">
+            <h5 class="m-0">Новый участок : </h5>
+          </div>
+
+          <div class="row ">
+            <div class="col ">
+              <div class="form m-3">
+                <div class="form-group">
+                  <label for="site-name">Название участка : </label>
+                  <input id="site-name" class="form-control" placeholder="Введите название участка" v-model="siteName"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="site-comments">Коментарии : </label>
+                  <input type="text" id="site-comments" class="form-control" placeholder="Введите комментарии" v-model="siteComments"/>
+                </div>
+              </div>
+              <div class="card-footer text-muted">
+                <div>
+                  <div class="d-flex">
+                    <button @click="goBack" class="btn btn-outline">Отменить</button>
+                    <button class="btn btn-success ml-auto" type="submit">Сохранить</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-            <label>siteComments</label>
-            <textarea class="form-control" rows="3" placeholder="Desert area"></textarea>
-        </div>
-       </form>
-       <button class="btn btn-outline-primary">Create</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name:'NewSite',
-}
-
+  name: 'NewSite',
+  data() {
+    return {
+      siteName: '',
+      siteComments: '',
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
 </script>
+<style scoped>
 
-<style>
-.createPage{
-    width: 40%;
-    margin: auto;
-}
 </style>
