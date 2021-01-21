4575b012d1a0befd1d5788fa116bbce18f5ffd8c
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-21 16:57:33 +0600

    Update ShowUser.vue

--- src/components/user/ShowUser.vue
+++ src/components/user/ShowUser.vue
@@ -1,55 +1,35 @@
 <template>
-  <d-container fluid class="main-content-container px-4 pb-4">
-    <d-row no-gutters class="page-header py-4">
-      <d-col lg="12" md="12">
+  <div class="container-fluid px-4 pb-4">
+    <div class="row py-4">
+      <div class="col-12" lg="12" md="12">
         <div class="card card-small mb-4 pt-3">
           <div class="card-header border-bottom text-center">
             <div class="mb-3 mx-auto"></div>
-            <!-- vertical table head example -->
-            <table class="table table-bordered" style="width: 100%">
+            <table
+                v-for="(user, index) in users"
+                :key="index"
+                class="table table-bordered"
+                style="width: 100%"
+            >
               <tr>
-                <th class="text-left">Фамилия:</th>
-                <td>{{ user.surname }}</td>
+                <th class="text-left">Пользователь:</th>
+                <td>{{ user.username }}</td>
               </tr>
               <tr>
-                <th class="text-left">Имя:</th>
-                <td>{{ user.name }}</td>
+                <th class="text-left">Зарегистировался:</th>
+                <td>{{ user.createdAt }}</td>
               </tr>
-              <tr>
-                <th class="text-left">Отчество:</th>
-                <td>{{ user.middle_name }}</td>
-              </tr>
-              <tr>
-                <th class="text-left">Должность:</th>
-                <td>{{ user.position }}</td>
-              </tr>
-              <!-- <tr>
-          <th class="text-left">Email:</th>
-          <td>{{ u.email }}</td>
-        </tr>
-        <tr>
-          <th class="text-left">Телефон:</th>
-          <td>{{ u.phone }}</td>
-        </tr>
-        <tr>
-          <th class="text-left">Должность:</th>
-          <td>{{ u.position }}</td>
-        </tr>
-        <tr>
-          <th class="text-left">Био:</th>
-          <td>{{ u.bio }}</td>
-        </tr> -->
             </table>
           </div>
           <div class="card-footer text-muted">
-            <d-button @click="goBack()" pill>
+            <button class="btn btn-secondary" @click="goBack()" pill>
               <a class="text-white"> &larr; Вернуться назад </a>
-            </d-button>
+            </button>
           </div>
         </div>
-      </d-col>
-    </d-row>
-  </d-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -57,14 +37,12 @@ export default {
   name: 'ShowUser',
   data() {
     return {
-      user: {
-        id: 1,
-        name: 'Иван',
-        surname: 'Иванович',
-        middle_name: 'Петрович',
-        position: 'Бригадир',
-        createdAt: '2011-10-22',
-      },
+      users: [
+        {
+          username: 'Иван Иванов',
+          createdAt: '2011-10-22',
+        },
+      ],
     };
   },
   methods: {
@@ -74,3 +52,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+</style>
