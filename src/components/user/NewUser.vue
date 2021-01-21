55cba9aae155658074ae92e47d694f0c590349c6
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-21 16:55:35 +0600

    Update NewUser.vue

--- src/components/user/NewUser.vue
+++ src/components/user/NewUser.vue
@@ -1,45 +1,39 @@
 <template>
-  <d-container fluid class="main-content-container px-4 pb-4">
-    <d-row no-gutters class="page-header py-4">
-      <d-col lg="12" md="12">
-        <d-card class="card-small">
-          <!-- Form Example -->
-          <d-card-header class="border-bottom">
-            <h6 class="m-0">Новый пользователь</h6>
-          </d-card-header>
-          <d-list-group flush>
-            <d-list-group-item class="p-3">
-              <d-row>
-                <d-col>
-                  <d-form>
+  <div class="container-fluid px-4 pb-4">
+    <div class="row">
+      <div class="col-12 md-12">
+        <div class="card-small">
+          <div class="card-header border-bottom">
+            <h5 class="m-0">Новый пользователь : </h5>
+          </div>
+
+              <div class="row ">
+                <div class="col ">
+                  <div class="form m-3">
                     <div class="form-group">
-                      <label>Имя:</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Иванов Иван"
-                      />
+                      <label for="user-name">Имя пользователя: </label>
+                      <input id="user-name" class="form-control" placeholder="Иван Иванов" v-model="userName"/>
                     </div>
+
                     <div class="form-group">
-                      <label>Должность:</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Директор"
-                      />
+                      <label for="user-comments">Должность : </label>
+                      <input type="text" id="user-comments" class="form-control" placeholder="Директор" v-model="userComments"/>
                     </div>
-                    <button type="submit" class="btn btn-primary">
-                      Добавить
-                    </button>
-                  </d-form>
-                </d-col>
-              </d-row>
-            </d-list-group-item>
-          </d-list-group>
-        </d-card>
-      </d-col>
-    </d-row>
-  </d-container>
+                  </div>
+                  <div class="card-footer text-muted">
+                    <div>
+                      <div class="d-flex">
+                        <button @click="goBack" class="btn btn-outline">Отменить</button>
+                        <button class="btn btn-success ml-auto" type="submit">Сохранить</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -47,18 +41,19 @@ export default {
   name: 'NewUser',
   data() {
     return {
-      newUser: {
-        title: '',
-        body: '',
-      },
+      userName: '',
+      userComments: '',
     };
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
   },
 };
 </script>
-
 <style scoped>
-.container {
-  margin: auto;
-  width: 50%;
+ul {
+  list-style: none;
 }
 </style>
