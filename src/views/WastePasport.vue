713de22460752e04fb13460c8f006857e3192f41
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-01-26 21:18:54 +0600

    Create WastePasport.vue
    Waste Pasport

---
+++ src/views/WastePasport.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-content-container container-fluid px-4">
+    <div class="page-header row no-gutters py-4">
+      <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+        <span class="text-uppercase page-subtitle">Паспорт отходов</span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card card-small mb-4">
+          <div class="card-body">
+            <WastePasportTable />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import WastePasportTable from '@/components/waste-pasport/WastePasportTable';
+export default {
+name: "WastePasport",
+  components: { WastePasportTable }
+}
+</script>
+
+<style scoped>
+
+</style>
