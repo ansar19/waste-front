b644dd3f759796d3c2a132c0eac27b54e466abfb
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-02-17 18:25:10 +0600

    Update EditUtilizer.vue

--- src/components/utilizer/EditUtilizer.vue
+++ src/components/utilizer/EditUtilizer.vue
@@ -5,50 +5,46 @@
         <d-card class="card-small">
           <!-- Form Example -->
           <d-card-header class="border-bottom">
-            <h6 class="m-0">Редактировать место утилизации отходов</h6>
+            <h6 class="m-0">Редактировать место утилизации отходов
+            </h6>
           </d-card-header>
-          <d-card-body>
-            <d-list-group flush>
-              <d-list-group-item class="p-3">
-                <d-row>
-                  <d-col>
-                    <d-form>
-                      <div class="form-group">
-                        <label for="utilizer-name"
-                          >Наименование места утилизации</label
-                        >
-                        <d-input
-                          id="utilizer-name"
-                          placeholder="Введите название места утилиизации"
-                          v-model="utilizePlaceName"
-                        />
-                      </div>
+          <div class="col s12 m6 mt-2">
+            <div>
+              <form @submit.prevent="submitHandler">
+                <div class="form-group">
+                  <label>Выберите место утилизации</label>
+                  <select ref="select3" v-model="current" class="form-control form-control-input">
+                    <option class="form-option" v-for="(title, id) in utilizators">{{title.title}}</option>
+                  </select>
+                </div>
 
-                      <!-- vue - select -->
-                      <div class="form-group">
-                        <label for="v-select">Тип места утилизации</label>
-                        <v-select
-                          id="v-select"
-                          label="utilizeTypeName"
-                          v-model="utilizeType"
-                          :options="utilizeTypeOptions"
-                        ></v-select>
-                        {{ utilizeTypeName }}
-                      </div>
-                      <!-- / vue - select -->
-                    </d-form>
-                  </d-col>
-                </d-row>
-              </d-list-group-item>
-            </d-list-group>
-          </d-card-body>
-          <div class="card-footer border-top">
-            <div class="d-flex">
-              <router-link to="/utilizers">Отменить</router-link>
-              <button class="btn btn-success ml-auto" type="submit">
-                Сохранить
-              </button>
+                <div class="form-group">
+                  <label for="name">Наименование места утилизации</label>
+                  <input id="name" type="text" v-model="title" class="form-control form-control-input">
+                </div>
+
+                <div class="from-group">
+                  <label for="phone">Контактный номер места утилизации</label>
+                  <input id="phone" type="text" v-model="utilizatorPhone" class="form-control form-control-input">
+                </div>
+
+                <div class="form-group">
+                  <label for="utilizator-bank-details">Банковские детали места утилизации</label>
+                  <textarea id="utilizator-bank-details" type="textarea" v-model="utilizatorBankDetail"
+                            class="form-control">
+          </textarea>
+                </div>
+
+
+                <button class="btn btn-success waves-effect waves-light mb-3" type="submit">
+                  Обновить
+                  <i class="material-icons right">send</i>
+                </button>
+              </form>
             </div>
+          </div>
+          <div class="card-footer border-top mb">
+
           </div>
         </d-card>
       </d-col>
@@ -57,22 +53,109 @@
 </template>
 
 <script>
+import localizeFilter from '@/filters/localize.filter'
 export default {
-  data() {
-    return {
-      utilizePlaceName: 'Полигон ТБО',
-      utilizeType: '',
-      utilizeTypeOptions: [
-        {
-          utilizeTypeCode: 'externalUtilization',
-          utilizeTypeName: 'Внешняя утилизация',
-        },
-        {
-          utilizeTypeCode: 'internalUtilization',
-          utilizeTypeName: 'Внутренняя утилизация',
-        },
-      ],
-    };
+  props: {
+
   },
-};
+  data: () => ({
+    select3: null,
+    title: 'ГКП "Полигон ТБО"',
+    utilizatorPhone: '+77787787878',
+    utilizatorBankDetail: 'АО "Каспий банк"',
+    current: null,
+    utilizators: [
+      {
+        id: 1,
+        title: 'ГКП "Полигон ТБО"'
+      },
+      {
+        id: 2,
+        title: 'ТОО "Цех переработки пластмасс"'
+      },
+      {
+        id: 3,
+        title: 'ТОО "Котельная №2"'
+      },
+      {
+        id: 4,
+        title: 'АО "Аккумуляторный завод"'
+      }
+    ]
+  }),
+  validations: {
+    title: {  },
+    utilizatorPhone : {  }
+  },
+  watch: {
+    current(utilId) {
+      const { title, utilizatorPhone, utilizatorBankDetail } = this.utilizators.find(u => u.id === utilId)
+      this.title = title,
+        this.utilizatorPhone = utilizatorPhone,
+        this.utilizatorBankDetail = utilizatorBankDetail
+    }
+  },
+  // created() {
+  //   const { id, title, utilizatorPhone, utilizatorBankDetail } = this.utilizators[0]
+  //   this.current = id
+  //   this.title = title
+  //   this.utilizatorPhone = utilizatorPhone,
+  //     this.utilizatorBankDetail
+  // },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      try {
+        const utilizatorData = {
+          id: this.current,
+          title: this.title,
+          utilizatorPhone: this.utilizatorPhone,
+          utilizatorBankDetail: this.utilizatorBankDetail
+        }
+        await this.$store.dispatch('updateUtilizator', utilizatorData)
+        this.$message(localizeFilter('Utilizator_HasBeenUpdated'))
+        this.$emit('updated', utilizatorData)
+      } catch (e) {}
+    }
+  },
+  // mounted() {
+  //   this.select3 = M.FormSelect.init(this.$refs.select3)
+  //   M.updateTextFields()
+  // },
+  destroyed() {
+    if (this.select3 && this.select3.destroy) {
+      this.select3.destroy()
+    }
+  }
+}
 </script>
+<style scoped>
+.form-control-input{
+  border: none;
+  border-radius: 0;
+  background-color: transparent;
+  border-bottom: 1px solid #9e9e9e;
+  margin-bottom: 10px;
+}
+.form-control-input:focus {
+  border-bottom: 1px solid #26a69a;
+  box-shadow: 0 1px 0 0 #26a69a;
+}
+label{
+  font-size: .8rem;
+  color: #9e9e9e;
+}
+.form-option{
+  font-size: 16px;
+  color: #26a69a;
+  display: block;
+  line-height: 22px;
+  padding: 14px 16px;
+}
+</style>
+
+
