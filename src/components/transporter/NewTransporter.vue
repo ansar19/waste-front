4376092b97cb209a67256cf1fd5da23631d11593
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-02-17 18:23:40 +0600

    Update NewTransporter.vue

--- src/components/transporter/NewTransporter.vue
+++ src/components/transporter/NewTransporter.vue
@@ -7,49 +7,35 @@
           <d-card-header class="border-bottom">
             <h6 class="m-0">Новый перевозчик</h6>
           </d-card-header>
-          <d-card-body>
-            <d-list-group flush>
-              <d-list-group-item class="p-3">
-                <d-row>
-                  <d-col>
-                    <d-form>
-                      <div class="form-group">
-                        <label for="transporter-name"
-                          >Наименование перевозчика</label
-                        >
-                        <d-input
-                          id="transporter-name"
-                          placeholder="Введите название перевозчика"
-                          v-model="transporterName"
-                        />
-                      </div>
 
-                      <!-- vue - select -->
-                      <div class="form-group">
-                        <label for="v-select">Тип перевозчика</label>
-                        <v-select
-                          id="v-select"
-                          label="transporterTypeName"
-                          v-model="transporterType"
-                          :options="transporterTypeOptions"
-                        ></v-select>
-                        {{ transporterTypeName }}
-                      </div>
-                      <!-- / vue - select -->
-                    </d-form>
-                  </d-col>
-                </d-row>
-              </d-list-group-item>
-            </d-list-group>
-          </d-card-body>
-          <d-card-footer class="border-top">
-            <div class="d-flex">
-              <router-link to="/utilizers">Отменить</router-link>
-              <button class="btn btn-success ml-auto" type="submit">
-                Сохранить
-              </button>
+          <div class="col s12 m6 mt-2">
+            <div>
+              <form @submit.prevent="submitHandler">
+                <div class="from-group">
+                  <label for="title-transporter">Наименование перевозчика</label>
+                  <input id="title-transporter" type="text" v-model="title" class="form-control form-control-input">
+                </div>
+
+                <div class="from-group">
+                  <label for="transporter-phone">Контактный номер перевозчика</label>
+                  <input id="transporter-phone" type="text" v-model="transporterPhone" class="form-control form-control-input">
+                </div>
+
+                <div class="form-group">
+                  <label for="transporter-bank-details">Банковские детали перевозчика</label>
+                  <textarea id="transporter-bank-details" type="textarea" v-model="transporterBankDetail" class="form-control"></textarea>
+                </div>
+
+                <button class="btn btn-success waves-effect waves-light mb-4" type="submit">
+                  Создать
+                  <i class="material-icons right">send</i>
+                </button>
+              </form>
             </div>
-          </d-card-footer>
+          </div>
+          <div class="card-footer border-top mb">
+
+          </div>
         </d-card>
       </d-col>
     </d-row>
@@ -57,27 +43,60 @@
 </template>
 
 <script>
+import localizeFilter from '@/filters/localize.filter'
+
 export default {
-  data() {
-    return {
-      transporterName: '',
-      transporterType: '',
-      transporterTypeOptions: [
-        {
-          transporterTypeCode: 'externalUtilization',
-          transporterTypeName: 'Внешняя утилизация',
-        },
-        {
-          transporterTypeCode: 'internalUtilization',
-          transporterTypeName: 'Внутренняя утилизация',
-        },
-      ],
-    };
+  data: () => ({
+    title: '',
+    transporterPhone: '',
+    transporterBankDetail: ''
+  }),
+  validations: {
+    title: {  },
+    transporterPhone: {  }
+  },
+  mounted() {
+    M.updateTextFields()
   },
   methods: {
-    goBack() {
-      this.$router.go(-1);
-    },
-  },
-};
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      try {
+        const transporter = await this.$store.dispatch('createTransporter', {
+          title: this.title,
+          transporterPhone: this.transporterPhone,
+          transporterBankDetail: this.transporterBankDetail
+        })
+        this.title = ''
+        this.transporterPhone = ''
+        this.transporterBankDetail = ''
+        this.$v.$reset()
+        this.$message(localizeFilter('Transporter_HasBeenCreated'))
+        this.$emit('created', transporter)
+      } catch (e) {}
+    }
+  }
+}
 </script>
+
+<style scoped>
+  .form-control-input{
+    border: none;
+    border-radius: 0;
+    background-color: transparent;
+    border-bottom: 1px solid #9e9e9e;
+    margin-bottom: 10px;
+  }
+  .form-control-input:focus {
+    border-bottom: 1px solid #26a69a;
+    box-shadow: 0 1px 0 0 #26a69a;
+  }
+  label{
+    font-size: .8rem;
+    color: #9e9e9e;
+  }
+</style>
