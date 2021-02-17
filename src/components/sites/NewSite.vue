837fb7c165c91238c199b346dd9db578bc73d6c0
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-02-17 18:00:50 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -5,62 +5,99 @@
         <d-card class="card-small">
           <!-- Form Example -->
           <d-card-header class="border-bottom">
-            <h6 class="m-0">Новый участок</h6>
+            <h6 class="m-0">Создать
+            </h6>
           </d-card-header>
-          <d-card-body>
-            <d-list-group flush>
-              <d-list-group-item class="p-3">
-                <d-row>
-                  <d-col>
-                    <d-form>
-                      <div class="form-group">
-                        <label for="site-name">Наименование участка: </label>
-                        <d-input
-                          id="site-name"
-                          placeholder="Введите название участка"
-                          v-model="siteName"
-                        />
-                      </div>
-                      <div class="form-group">
-                        <label for="site-comments">Комментарии: </label>
-                        <d-input
-                          id="site-comments"
-                          placeholder="Введите комментарии"
-                          v-model="siteComments"
-                        />
-                      </div>
-                    </d-form>
-                  </d-col>
-                </d-row>
-              </d-list-group-item>
-            </d-list-group>
-          </d-card-body>
-          <d-card-footer class="border-top">
-            <div class="d-flex">
-              <router-link to="/sites">Отменить</router-link>
-              <button class="btn btn-success ml-auto" type="submit">
-                Сохранить
-              </button>
+          <div class="col s12 m6 mt-2">
+            <div>
+
+              <form @submit.prevent="submitHandler">
+                <div class="from-group">
+                  <label for="name1">Наименование участка</label>
+                  <input id="name1" type="text" v-model="siteName" class="form-control form-control-input">
+                </div>
+
+                <div class="form-group">
+                  <label for="site-comments1">Комментарии к участку</label>
+                  <textarea id="site-comments1" type="textarea" v-model="siteComments"
+                            class="form-control">
+          </textarea>
+                </div>
+
+                <button class="btn btn-success waves-effect waves-light mb-3" type="submit">
+                  Создать
+                  <i class="material-icons right">send</i>
+                </button>
+              </form>
             </div>
-          </d-card-footer>
+          </div>
+          <div class="card-footer border-top mb">
+
+          </div>
         </d-card>
       </d-col>
     </d-row>
   </d-container>
+
 </template>
 
 <script>
+import localizeFilter from '@/filters/localize.filter'
+
 export default {
-  data() {
-    return {
-      siteName: '',
-      siteComments: '',
-    };
+  data: () => ({
+    siteName: '',
+    siteComments: ''
+  }),
+  validations: {
+    siteName: {  }
   },
+  // mounted() {
+  //   M.updateTextFields()
+  // },
   methods: {
-    goBack() {
-      this.$router.go(-1);
-    },
-  },
-};
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      try {
+        const site = await this.$store.dispatch('createSite', {
+          siteName: this.siteName,
+          siteComments: this.siteComments
+        })
+        this.siteName = ''
+        this.siteComments = ''
+        this.$v.$reset()
+        this.$message(localizeFilter('Site_HasBeenCreated'))
+        this.$emit('created', site)
+      } catch (e) {}
+    }
+  }
+}
 </script>
+<style scoped>
+.form-control-input{
+  border: none;
+  border-radius: 0;
+  background-color: transparent;
+  border-bottom: 1px solid #9e9e9e;
+  margin-bottom: 10px;
+}
+.form-control-input:focus {
+  border-bottom: 1px solid #26a69a;
+  box-shadow: 0 1px 0 0 #26a69a;
+}
+label{
+  font-size: .8rem;
+  color: #9e9e9e;
+}
+.form-option{
+  font-size: 16px;
+  color: #26a69a;
+  display: block;
+  line-height: 22px;
+  padding: 14px 16px;
+}
+</style>
